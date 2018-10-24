2753cc7d3180e641e9a08488eb9076498676439c
Aprinaldi <qlink186@ymail.com>
2018-10-24 11:05:02 +0700

    181025-update-download_area

--- db/181011-dk_tpi-d_download-128.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `d_download` (`id_download`, `id_opd`, `id_user`, `nama_file`, `deskripsi_file`, `id_jenis_file`, `id_kat_file`, `status_file`, `tampil_opd`, `link_file`, `tgl_file`, `tgl_upload`, `tgl_edit`, `ket_file`, `yg_ngubah`) VALUES
-	
-	(128, 516000000, 1, 'F-A01-Formulir Pendataan Sistem Elektronik', 'Formulir Pendataan Sistem Elektronik di Lingkungan Pemerintah Kota Tanjungpinang', 15, 1, '1', '1', 'F-A01-Formulir_Isian_Pendataan_Sistem_Elektronik.xlsx', '2018-10-11 23:39:06', '2018-10-11 23:39:11', '2018-10-11 23:39:13', 'Diskominfo', NULL);
